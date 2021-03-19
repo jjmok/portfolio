adf96fdcf76e1fdddf60069122541b61b66c5d6f
jjmok89 <jjmok89@gmail.com>
2021-03-19 17:28:40 +0800

    Added more info

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <div class="p-5">
+    <h2>Hi! my name is Jee Jin. Welcome to my portfolio site!</h2>
+    <p>This is a work in progress</p>
+  
+    <!-- <h2>Case Study</h2>
+    <ul>
+      <li><a target="_blank" href="opogo.html">Opogo Case Study</a></li>
+    </ul>
+    
+  
+    <h2>Works</h2>
+    <p>As an Illustrator</p>
+
+    <p>As UI Developer</p>
+    <ul>
+      <li>Design System Management</li>
+    </ul>
+  
+    <p>As Junior Product Designer</p>
+    <ul>
+      <li>Colors and Font Scheme</li>
+      <li>Tables</li>
+    </ul>
+  
+    <h2>CSS Tutorial Training</h2>
+    <ul>
+      <li><a target="_blank" href="https://slides.com/jjmok/css">CSS Basics</a></li>
+      <li><a target="_blank" href="https://slides.com/jjmok/layouts-new">Flex Layouts</a></li>
+      <li><a target="_blank" href="https://slides.com/jjmok/csspractices">CSS Best Practices</a></li>
+    </ul>
+  
+    <h2>Creative</h2>
+    <a target="_blank" href="https://codepen.io/JayJay89/full/OKOdma">Charles the CSS Corgi</a> -->
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .img-500 {
+    width: 500px;
+  }
+
+  body { font-family: 'Poppins', sans-serif; }
+
+  h1,h2,h3,h4,h5,h6 {
+    font-family: 'Rubik', sans-serif;
+  }
+  .img-500 {
+    width: 500px;
+  }
+
+  .img-700 {
+    width: 700px;
+  }
 </style>
